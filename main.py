104b541d4893871598674dc03e6b34a66ac70fab
Ezaldeen Alayed <ezaldeena@latro.com>
2026-03-29 17:15:25 +0300

    add tax

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ def add(a, b):
     return a + b
 
 
+def add_tax(amount, tax_rate):
+    return amount + (amount * tax_rate / 100)
+
+
 if __name__ == "__main__":
     print("Sum:", add(10, 5))
+    print("With tax:", add_tax(100, 15))
